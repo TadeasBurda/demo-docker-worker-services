446b76e85017b45997c27659dfa1c5ebb10a8438
Tadeáš Burda <tadeas.burda@outlook.com>
2025-03-08 15:10:19 +0100

    Add image digest retrieval and versioning to build.sh
    Updated `build.sh` to retrieve Docker image digests for `workerservice1` and `workerservice2`. The digests are saved to `versions.txt`, which is uploaded to the FTP server along with the Docker images. The script now also cleans up by removing `versions.txt` after the upload.

--- Demo docker worker services/build.sh
+++ Demo docker worker services/build.sh
@@ -18,10 +18,20 @@ docker-compose build
 docker save workerservice1:latest | gzip > workerservice1_latest.tar.gz
 docker save workerservice2:latest | gzip > workerservice2_latest.tar.gz
 
-# Upload to FTP server
+# Get image digests
+WORKERSERVICE1_DIGEST=$(docker inspect --format='{{index .Id}}' workerservice1:latest)
+WORKERSERVICE2_DIGEST=$(docker inspect --format='{{index .Id}}' workerservice2:latest)
+
+# Save versions to a file
+echo "workerservice1:$WORKERSERVICE1_DIGEST" > versions.txt
+echo "workerservice2:$WORKERSERVICE2_DIGEST" >> versions.txt
+
+# Upload Docker images and versions file to FTP server
 curl -T workerservice1_latest.tar.gz ftp://$FTP_SERVER/docker_images/ --user $USERNAME:$PASSWORD
 curl -T workerservice2_latest.tar.gz ftp://$FTP_SERVER/docker_images/ --user $USERNAME:$PASSWORD
+curl -T versions.txt ftp://$FTP_SERVER/docker_images/ --user $USERNAME:$PASSWORD
 
 # Remove local files after upload
 rm workerservice1_latest.tar.gz
 rm workerservice2_latest.tar.gz
+rm versions.txt
