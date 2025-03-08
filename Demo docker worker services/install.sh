22e6074781b2d67bb825b36d86c5c0e5e66e1bc4
Tadeáš Burda <tadeas.burda@outlook.com>
2025-03-08 15:11:51 +0100

    Enhance Docker image management in install.sh
    Updated the script to check for a local versions file and compare it with the latest from the FTP server. If updates are available, new Docker images are downloaded and the local versions file is updated. If the local file is missing, the latest versions file and corresponding images are downloaded. Removed the previous method of downloading specific images and ensured the latest versions file is saved locally.

--- Demo docker worker services/install.sh
+++ Demo docker worker services/install.sh
@@ -11,17 +11,49 @@ FTP_SERVER=$1
 USERNAME=$2
 PASSWORD=$3
 
-# Download Docker images
-curl -o workerservice1_latest.tar.gz ftp://$FTP_SERVER/docker_images/workerservice1_latest.tar.gz --user $USERNAME:$PASSWORD
-curl -o workerservice2_latest.tar.gz ftp://$FTP_SERVER/docker_images/workerservice2_latest.tar.gz --user $USERNAME:$PASSWORD
+# Download the latest versions file from FTP server
+curl -o versions_latest.txt ftp://$FTP_SERVER/docker_images/versions.txt --user $USERNAME:$PASSWORD
 
-# Load Docker images
-docker load -i workerservice1_latest.tar.gz
-docker load -i workerservice2_latest.tar.gz
+# Check if the versions file exists locally
+if [ -f versions.txt ]; then
+    # Compare the local versions with the latest versions
+    if ! cmp -s versions.txt versions_latest.txt; then
+        echo "New versions available. Updating..."
+
+        # Download the latest Docker images
+        while IFS= read -r line; do
+            service=$(echo $line | cut -d':' -f1)
+            version=$(echo $line | cut -d':' -f2)
+            curl -o ${service}_${version}.tar.gz ftp://$FTP_SERVER/docker_images/${service}_${version}.tar.gz --user $USERNAME:$PASSWORD
+            docker load -i ${service}_${version}.tar.gz
+            rm ${service}_${version}.tar.gz
+        done < versions_latest.txt
+
+        # Update the local versions file
+        mv versions_latest.txt versions.txt
+
+        echo "Update completed."
+    else
+        echo "No updates available."
+        rm versions_latest.txt
+    fi
+else
+    echo "Local versions file not found. Downloading the latest versions..."
+
+    # Download the latest Docker images
+    while IFS= read -r line; do
+        service=$(echo $line | cut -d':' -f1)
+        version=$(echo $line | cut -d':' -f2)
+        curl -o ${service}_${version}.tar.gz ftp://$FTP_SERVER/docker_images/${service}_${version}.tar.gz --user $USERNAME:$PASSWORD
+        docker load -i ${service}_${version}.tar.gz
+        rm ${service}_${version}.tar.gz
+    done < versions_latest.txt
+
+    # Save the latest versions file locally
+    mv versions_latest.txt versions.txt
+
+    echo "Initial download and update completed."
+fi
 
 # Run Docker containers
 docker-compose up
-
-# Remove local files after upload
-rm workerservice1_latest.tar.gz
-rm workerservice2_latest.tar.gz
